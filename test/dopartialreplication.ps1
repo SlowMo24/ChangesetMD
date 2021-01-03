dae10a23f615766414709c557312a9e5acfcd6b4
Pierre Beland <infosbelas-am@yahoo.fr>
2021-01-02 23:26:27 -0500

    document PostgreSQL autorisations and correct changesetMD.py call

--- test/dopartialreplication.ps1
+++ test/dopartialreplication.ps1
@@ -1,10 +1,11 @@
 <# 
-Test Create schema and do Partial Replicate from Planet Changesets repository using fromseq and toseq parameters
+Test to Create PosgreSQL Db, extensions, schema and do Partial Replicate from Planet Changesets repository using fromseq and toseq parameters
 Windows Powershell script
-@author Pierre BÃ©land 2021
-	-- createdb osm_qual
-	-- psql -d osm_qual -c "CREATE EXTENSION postgis"
-	-- psql -d osm_qual -c "CREATE EXTENSION hstore"
+@author Pierre Béland 2021
+
+For documentation, lines below and in changesetMD.py contain Db Instructions to create database, extensions and schemas. 
+Your Db user account need to have have the rights to create these various objects. 
+If you encounter problems, you should contact your Db administrator to provide you rights or run the script to create these objects.
 #>
 clear
 $OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
@@ -15,12 +16,12 @@ Write-host
 
 # note if restrictions to create on your Database, assure that the Admin creates / gives access authorisation
 
-# comment the following two lines after the Database, schema and tables are created
+# comment the next two lines after the db, schema and tables created
 createdb -h localhost -p 7513 -U osm "changesetMD"
-python changesetMD.py -H localhost -P 7513 -u osm -d changesetMD --schema=t_dopartial --create --geometry
+python changesetMD.py -H localhost -P 7513 -u osm -d changesetMD --schema=test_dopartial --create --geometry
 
 <#
-test Partial replication
+to test Partial replication
 we suggest a difference of 50 between fromseq and toseq
 osm_changeset_state table is not used and modified by running this script
 ---
@@ -29,7 +30,8 @@ Example: sequences from 4263076 to 4263126
 The bulkrows Â«throttleÂ» parameter is for Bulk rows insert / commit to reduce acess to the PostgreSQL Db. Data is kep in Arrays. Check for any impact on the memory used by the application.
 #>
 
-python changesetMD.py -H localhost -P 7513 -u osm -d changesetMD --schema=t_dopartial --replicate --geometry --bulkrows=1000 --fromseq=4263076 --toseq=4263166
+# running the following line, if log error «Relation do not exist» might indicate that the postgres user do not have right to access the tables - if such case arize, you must consult your Db administrator
+python changesetMD.py -H 'localhost' -P 7513 -u 'osm' --database='changesetMd' --schema=test_dopartial --replicate --geometry --bulkrows=1000 --fromseq=4263076 --toseq=4263126
 
 Write-host
 Write-host "====================   changesetMD /> ===================="
