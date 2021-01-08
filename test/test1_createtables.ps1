b16a1976897d2c83841ac84921d45e835f382287
Pierre Beland <infosbelas-am@yahoo.fr>
2021-01-07 21:47:12 -0500

    Create test1_createtables.ps1
    Create PostgreSQL Database, schema and tables

---
+++ test/test1_createtables.ps1
@@ -0,0 +1,32 @@
+<# 
+changesetMD - Create PostgreSQL Database, schema and tables
+
+This Test script documents the various steps  to create database, extensions and schemas.
+NOTE : If you add --geometry option, Superuser Role is necessary to create the POSTGIS extension.
+You can ask the Superuser to run this script for you
+
+Windows Powershell script
+@author Pierre Béland 2021
+#>
+clear
+$OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+#
+# specify directory where you stored the changesetMD python modules
+#SET PATH="C:/Program Files/PostgreSQL/13/bin";%PATH%
+Set-Location "D:\OsmContributorStats\changesetMD_p3\github"
+
+
+Write-host "==================== < changesetMD    ===================="
+Write-host -BackgroundColor white -ForegroundColor blue " test1_createtables.ps1                             "
+Write-host
+Write-host -ForegroundColor white " Create database                                    "
+
+$error.clear()
+try   {createdb -h localhost -p 5432 -U osm "changesetmd_test"}
+catch {Write-host -BackgroundColor white -ForegroundColor red " Create database error occured: " $_.Exception.Message }
+
+python changesetMD.py -H localhost -P 5432 -u osm -d changesetmd_test --schema=testfile --create --geometry
+
+Write-host
+Write-host "====================   changesetMD /> ===================="
