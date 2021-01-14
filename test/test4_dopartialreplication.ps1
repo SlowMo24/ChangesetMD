b46f3cb8703737b02b4358e9a1f6d9b9b3679e9e
Pierre Beland <infosbelas-am@yahoo.fr>
2021-01-14 14:01:15 -0500

    Documentation + Relative paths

--- test/test4_dopartialreplication.ps1
+++ test/test4_dopartialreplication.ps1
@@ -1,7 +1,8 @@
 <# 
 changesetMD - Test4 - doPartialReplication
 
-This Test script documents the various steps  to Parse an OSM Replication file in the database.
+Partially Replicates  Planet Changesets in the postgreSQL database wit parameters fromseq and toseq
+The python option [-u] reduces memory usage as it forces the stdout and stderr streams to be unbuffered 
 
 Windows Powershell script
 @author Pierre Béland 2021
@@ -10,13 +11,12 @@ clear
 $OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 #
-# specify directory where you stored the changesetMD python modules
-Set-Location "D:\OsmContributorStats\changesetMD_p3\github"
-
+# directory where the changesetMD python modules are stored
+cd ..
 
 Write-host "==================== < changesetMD    ===================="
 Write-host -BackgroundColor white -ForegroundColor blue "test4_dopartialreplication.ps1                            "
-Write-host
+Write-host " "
 
 Write-host -ForegroundColor cyan "Delete tables rows prior to Test"
 psql -h localhost -p 5432 -U osm -d changesetmd_test -c "delete from testfile.osm_changeset;"
@@ -24,7 +24,6 @@ psql -h localhost -p 5432 -U osm -d changesetmd_test -c "delete from testfile.os
 
 Write-host -ForegroundColor cyan "Partial Replication "
 python -u -X utf8 changesetMD.py -H 'localhost' -P 5432 -u 'osm' --database='changesetmd_test' --schema=testfile --replicate --geometry --bulkrows=500 --fromseq=4260811 --toseq=4261001
-
 
 Write-host
 Write-host "====================   changesetMD /> ===================="
