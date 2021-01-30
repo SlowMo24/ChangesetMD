30963b6698e6b4e9c862b0d8179824e0121d0d3c
Pierre Beland <infosbelas-am@yahoo.fr>
2021-01-30 11:10:52 -0500

    option bz2buffer

--- test/test2_parsefile.sh
+++ test/test2_parsefile.sh
@@ -21,7 +21,7 @@ psql -h localhost -p 5432 -U osm  -d changesetmd_test -c "drop schema if exists 
 echo "Parses large file, osm or osm.bz2  (Insert without indexes)"
 python3  -u -X utf8 changesetmd.py -H localhost -P 5432 -u osm -p osm -d changesetmd_test --schema=testfile --create --geometry --bulkrows=500 --logfile --file="test/changesets_testfile.osm"
 # instruction to Parse latest bz2 file located in planet subdirectory
-#python  -u -X utf8 changesetmd.py -H localhost -P 5432 -u osm -p osm -d changesetmd_test --schema=testfile --trunc --geometry --bulkrows=500000 --logfile --file=planet/changesets-latest.osm.bz2
+#python  -u -X utf8 changesetmd.py -H localhost -P 5432 -u osm -p osm -d changesetmd_test --schema=testfile --trunc --geometry --bulkrows=500000 --bz2buffer=500000 --logfile --file=planet/changesets-latest.osm.bz2
 
 echo "\n====================   changesetMD /> ===================="
 #back to test directory
